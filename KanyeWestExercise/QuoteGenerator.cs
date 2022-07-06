f948c4ee56058025960e0e6899e674fa6036a982
Elijah Lennon <elijahlennon1@gmail.com>
2022-07-05 20:54:20 -0500

    Completed Exercise

---
+++ KanyeWestExercise/QuoteGenerator.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+
+namespace KanyeWest
+{
+    public class QuoteGenerator
+    {
+        private HttpClient _client;
+
+        public QuoteGenerator(HttpClient client)
+        {
+            _client = client;
+        }
+
+        public string KanyeWest()
+        {
+            //API Url
+            var kanyeURL = "https://api.kanye.rest";
+
+            //Stores the JSON repsonse in a variable
+            var kanyeResponse = _client.GetStringAsync(kanyeURL).Result;
+
+            //Parses through the response we received to get the value associated with
+            //the NAME "quote"
+            var kanyeQuote = JObject.Parse(kanyeResponse).GetValue("quote").ToString();
+
+            return kanyeQuote;
+        }
+
+        public string RonSwanson()
+        {
+            //API Url
+            var ronURL = "https://ron-swanson-quotes.herokuapp.com/v2/quotes";
+
+            //Stores the JSON repsonse in a variable
+            var ronResponse = _client.GetStringAsync(ronURL).Result;
+
+            //Parses through the response we received to get the value associated with
+            //the NAME "quote"
+            var ronQuote = JArray.Parse(ronResponse);
+
+            return ronQuote[0].ToString();
+        }
+    }
+}
